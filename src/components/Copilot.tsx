fbd8ebd8a5be461aab5e9d461eb1ff0f1e986a2d
Julian <tipler.julian@gmail.com>
2024-05-06 11:20:18 -0500

    Copilot and CopilotForm and LoginModal component

---
+++ src/components/Copilot.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import supabase from "../clients/supabase";
+
+export const Copilot = () => {
+  const [copilot, setCopilot] = useState<any>({});
+  const [searchParams] = useSearchParams();
+  const copilotId = searchParams.get("copilot-id");
+
+  useEffect(() => {
+    if (copilotId) {
+      const fetchCopilot = async () => {
+        console.log(supabase);
+        const { data, error } = await supabase
+          .from("copilots")
+          .select("*")
+          .eq("id", copilotId)
+          .single();
+        if (error) {
+          console.error("Error fetching copilot", error);
+        }
+        if (data) {
+          setCopilot(data);
+        }
+      };
+      fetchCopilot();
+    }
+  }, [copilotId]);
+
+  if (!copilotId) return <div>Example Copilot Here</div>;
+  return <div>Copilot ID:{copilot.id}</div>;
+};
