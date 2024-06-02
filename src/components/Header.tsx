c927a9cc16d8843a6db2176c4fdd66e136f66b6c
zoeschmitt <zoeschmitt@hotmail.com>
2024-06-02 13:35:16 -0500

    feat: sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export const Header = () => {
   };
 
   return (
-    <header>
+    <header className="sticky top-0 left-0 right-0 z-10">
       <nav className="bg-white border-primary-border px-4 lg:px-6 py-2.5 dark:bg-gray-800 border-b">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to={"/"} className="flex items-center">
