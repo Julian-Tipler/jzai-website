9368423483c1d459c274aa68dcdd936866d4777b
zoeschmitt <zoeschmitt@hotmail.com>
2024-06-19 21:40:19 -0500

    fix: updated JZAI instances to WebPilot

--- src/(public)/layout.tsx
+++ src/(public)/layout.tsx
@@ -11,7 +11,7 @@ export const PublicLayout = () => {
   return (
     <div>
       <Helmet>
-        <title>{currentMatch.handle?.data.title} | JZAI</title>
+        <title>{currentMatch.handle?.data.title} | WisePilot</title>
       </Helmet>
       <Header />
       <main id="main-content">
